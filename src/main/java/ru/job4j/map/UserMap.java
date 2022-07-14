11ec719ee917ef84a3f78e440a90d7ad90ef5284
Jeka <masl.ev@mail.ru>
2022-07-14 16:22:55 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/UserMap.java
+++ src/main/java/ru/job4j/map/UserMap.java
@@ -15,7 +15,7 @@ public class UserMap {
         calendar.set(1991, 10, 13);
         User user1 = new User("Evgeny", 1, calendar);
         User user2 = new User("Evgeny", 1, calendar);
-        Map<User, Object> map = new HashMap<>() ;
+        Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
         for (Map.Entry<User, Object> pair : map.entrySet()) {
