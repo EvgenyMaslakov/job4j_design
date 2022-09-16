a572727d5dc8e3bd28a7f2efb70c7c04881ab2a8
Jeka <masl.ev@mail.ru>
2022-09-16 21:49:28 +0300

    0.1. FileOutputStream. [#252488 #344011]

--- src/main/java/ru/job4j/io/Multiple.java
+++ src/main/java/ru/job4j/io/Multiple.java
@@ -4,34 +4,15 @@ import java.io.FileOutputStream;
 
 public class Multiple {
     public static void main(String[] args) {
-        System.out.println("1 * 1 = 1");
-        System.out.println("1 * 2 = 2");
-        System.out.println("1 * 3 = 3");
-        System.out.println("1 * 4 = 4");
-        System.out.println("1 * 5 = 5");
-        System.out.println("1 * 6 = 6");
-        System.out.println("1 * 7 = 7");
-        System.out.println("1 * 8 = 8");
-        System.out.println("1 * 9 = 9");
         try (FileOutputStream out = new FileOutputStream("Multiple.txt")) {
-            out.write("1 * 1 = 1".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1 * 2 = 2".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1 * 3 = 3".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1 * 4 = 4".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1 * 5 = 5".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1 * 6 = 6".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1 * 7 = 7".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1 * 8 = 8".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1 * 9 = 9".getBytes());
-            out.write(System.lineSeparator().getBytes());
+            for (int i = 1; i <= 9; i++) {
+                for (int j = 1; j <= 9; j++) {
+                    System.out.println();
+                    String s = i + "*" + j + "=" + i * j + " ";
+                    out.write(s.getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
