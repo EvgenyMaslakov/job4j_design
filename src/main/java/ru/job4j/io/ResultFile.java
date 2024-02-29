dcdc734c6b87befc01b149109b2a5d05af4c0fa6
Evgeny <masl.ev@mail.ru>
2024-02-29 07:50:42 +0300

    2.0. FileOutputStream

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,22 +1,13 @@
 package ru.job4j.io;
 
-import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-/**
- * 0.4. BufferedOutputStream [#252490]
- */
 public class ResultFile {
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream("result.txt")
-                ))) {
-            out.println("Hello, world!");
-        } catch (IOException e) {
-            System.out.println("Ошибка при вводе данных в файл!");
+        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
+            output.write("Hello, world!".getBytes());
+            output.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
