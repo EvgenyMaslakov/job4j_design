e457a18b0709f02460a075f947b9636e2491080f
Evgeny <masl.ev@mail.ru>
2024-03-09 13:06:15 +0300

    12. Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,10 +9,11 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
+        validation(args);
+        Path start = Paths.get(args[0]);
         search(start, path -> path.toFile()
                 .getName()
-                .endsWith(".js"))
+                .endsWith(args[1]))
                 .forEach(System.out::println);
     }
 
@@ -20,5 +21,17 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validation(String[] args) throws IOException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Program arguments are empty or incomplete");
+        }
+        if ("С:\'".equals(args[0])) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if ("js".equals(args[1])) {
+            throw new IllegalArgumentException("The file extension is unspecified or incorrect");
+        }
     }
 }
