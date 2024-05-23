b5014ad3c10de02f8dc4ff5fe73b9aadba0e9cb6
Evgeny <masl.ev@mail.ru>
2024-05-23 07:46:57 +0300

    2. Формат JSON

--- src/main/java/ru/job4j/serialization/json/Main.java
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -6,6 +6,7 @@ import com.google.gson.GsonBuilder;
 /**
  * Создаем объект.
  * Преобразуем объект person в json-строку.
+ * Создаём новую json-строку с модифицированными данными
  * Превращаем json-строку обратно в объект
  */
 public class Main {
@@ -14,7 +15,18 @@ public class Main {
                 new String[] {"Worker", "Married"});
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(person));
-        final Person personMod = gson.fromJson(gson.toJson(person), Person.class);
+        final String personJson =
+                "{"
+                        + "\"sex\":false,"
+                        + "\"age\":35,"
+                        + "\"contact\":"
+                        + "{"
+                        + "\"phone\":\"+7(924)111-111-11-11\""
+                        + "},"
+                        + "\"statuses\":"
+                        + "[\"Student\",\"Free\"]"
+                        + "}";
+        final Person personMod = gson.fromJson(personJson, Person.class);
         System.out.println(personMod);
     }
 }
